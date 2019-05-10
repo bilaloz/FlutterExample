da65ef2aad9b1a8919972cac989f0c15ce41994c
Bilal OZ <152805013@ogr.cbu.edu.tr>
2019-05-10 14:46:00 +0300

    Route and navigasyon operation

---
+++ lib/listeler/navigasyon.dart
@@ -0,0 +1,217 @@
+import 'package:flutter/material.dart';
+
+class Navigasyon extends StatelessWidget {
+  String baslik = "BNavigator";
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Navigator",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                child: Text("Page A Link"),
+                color: Colors.deepOrange,
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ANavigasyon(),
+                      ));
+                },
+              ),
+              RaisedButton(
+                child: Text("Page B Link"),
+                color: Colors.red,
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => BNavigasyon(baslik: baslik),
+                      ));
+                },
+              ),
+              RaisedButton(
+                child: Text("Page c Link"),
+                color: Colors.blue,
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CNavigasyon(),
+                      ));
+                },
+              ),
+              RaisedButton(
+                child: Text("Page D get Data"),
+                color: Colors.green,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => DNavigasyon()),
+                  ).then((popThan) {
+                    debugPrint("Pop Than $popThan");
+                  });
+                },
+              ),
+              RaisedButton(
+                child: Text("Page A get Data"),
+                color: Colors.green,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ENavigasyon()),
+                  ).then((popThan) {
+                    debugPrint("Pop Than $popThan");
+                  });
+                },
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+class ANavigasyon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "A Navigator",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Page A Link"),
+            ],
+          ),
+        ));
+  }
+}
+
+class BNavigasyon extends StatelessWidget {
+  String baslik;
+
+  BNavigasyon({this.baslik});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "B Navigator",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Page A Link"),
+            ],
+          ),
+        ));
+  }
+}
+
+class CNavigasyon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "C Navigator",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Page C Link"),
+              RaisedButton(
+                color: Colors.purple,
+                child: Text("Geri Dön"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+class DNavigasyon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return WillPopScope(
+        onWillPop: () {
+          debugPrint("On will pop run");
+          Navigator.pop(context, false);
+          return Future.value(false);
+        },
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text(
+                "D Navigator",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Page DLink"),
+                  RaisedButton(
+                    color: Colors.purple,
+                    child: Text("Geri Dön"),
+                    onPressed: () {
+                      Navigator.pop(context, true);
+                    },
+                  )
+                ],
+              ),
+            )));
+  }
+}
+
+class ENavigasyon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "E Navigator",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(onPressed: () {
+                Navigator.pushReplacementNamed(context, '/BNavigasyon');
+              })
+            ],
+          ),
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,26 @@ import 'package:flutter_example/listeler/custom_scrool.dart';
 import 'package:flutter_example/listeler/gridview.dart';
 import 'package:flutter_example/listeler/list.dart';
 import 'package:flutter_example/listeler/list_type.dart';
+import 'package:flutter_example/listeler/navigasyon.dart';
 
 void main() {
   runApp(MaterialApp(
     title: 'Flutter Example ',
     debugShowCheckedModeBanner: false,
+    routes: {
+      '/'     :(context) => Navigasyon(),
+      '/CPage':(context) => DNavigasyon(),
+      '/DPage':(context) => DNavigasyon(),
+      '/APage':(context) => DNavigasyon(),
+    },
     theme: ThemeData(primarySwatch: Colors.orange,
       //brightness: Brightness.light,
     ),
-    home: Scaffold(
-      //appBar: AppBar(
-      //title: Text("Listeler",style:TextStyle(color: Colors.black),),
-      //),
-      body: CustomScrool(),
-    ),
+    //home: Scaffold(
+    //  //appBar: AppBar(
+    //  //title: Text("Listeler",style:TextStyle(color: Colors.black),),
+    //  //),
+    //  body: Navigasyon(),
+    //),
   ));
 }
